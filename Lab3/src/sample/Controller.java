f7533ca43e4c02619d6ede87c0af05888fc6fbba
Olgierd Tymosiewicz <tymek1667@gmail.com>
2021-04-23 10:43:27 +0200

    [LAB-3] Zadanie 5

--- Lab3/src/sample/Controller.java
+++ Lab3/src/sample/Controller.java
@@ -2,9 +2,11 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class Controller {
     public Button btnMain;
+    public TextField txtLicznik;
     int clickCounter = 0;
 
     public void onBtnAction(ActionEvent actionEvent) {
@@ -12,5 +14,6 @@ public class Controller {
         if(clickCounter == 1) {
             btnMain.setText("I jeszcze raz");
         }
+        txtLicznik.setText(String.valueOf(clickCounter));
     }
 }
