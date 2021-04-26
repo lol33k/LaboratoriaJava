ca010dc89decebdd790042f49d61f3a6674ea5d6
Olgierd Tymosiewicz <tymek1667@gmail.com>
2021-04-26 12:48:42 +0200

    [LAB-3] Zadanie 6

--- Lab3/src/sample/Controller.java
+++ Lab3/src/sample/Controller.java
@@ -4,16 +4,28 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+import static java.lang.Math.PI;
+
 public class Controller {
-    public Button btnMain;
-    public TextField txtLicznik;
-    int clickCounter = 0;
+
+    public Button btnCalculate;
+    public TextField radius;
+    public TextField height;
+    public TextField thickness;
+    public TextField result;
+    public TextField price1;
+    public TextField price2;
+
 
     public void onBtnAction(ActionEvent actionEvent) {
-        System.out.println("Kliknięcie nr " + ++clickCounter);
-        if(clickCounter == 1) {
-            btnMain.setText("I jeszcze raz");
-        }
-        txtLicznik.setText(String.valueOf(clickCounter));
+        double radiusValue = Double.parseDouble(radius.getText());
+        double heightValue = Double.parseDouble(height.getText());
+        double thicknessValue = Double.parseDouble(thickness.getText());
+
+        double resultValue = (PI * Math.pow((radiusValue + thicknessValue), 2) * heightValue) - (PI * Math.pow(radiusValue, 2) * heightValue);
+
+        result.setText(resultValue + "cm");
+        price1.setText(String.format("%.2f", resultValue * 4) + "zł");
+        price2.setText(String.format("%.2f", resultValue * 8) + "zł");
     }
 }
