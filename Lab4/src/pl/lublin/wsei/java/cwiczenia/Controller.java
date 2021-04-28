69cec64307a62ab3fb1c46fa180782dfb6320eec
Olgierd Tymosiewicz <tymek1667@gmail.com>
2021-04-28 15:14:38 +0200

    [LAB-4b] Zadanie 5b/5c

--- Lab4/src/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ Lab4/src/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -1,11 +1,14 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 
@@ -25,6 +28,23 @@ public class Controller {
 
     public void initialize() {
         fileChooser.getExtensionFilters().add(xmlFilter);
+        lstInfografiki.getSelectionModel().selectedIndexProperty().addListener(
+                new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> observableValue, Number oldVal, Number newVal) {
+                        int index = newVal.intValue();
+                        if(index !=-1) {
+                            txtAdresStrony.setText(igList.infografiki.get(index).adresStrony);
+                            Image image = new Image(igList.infografiki.get(index).adresMiniaturki);
+                            imgMiniaturka.setImage(image);
+                        }
+                        else {
+                            txtAdresStrony.setText("");
+                            imgMiniaturka.setImage(null);
+                        }
+                    }
+                }
+        );
     }
 
     public void btnOpenFileAction(ActionEvent actionEvent) {
