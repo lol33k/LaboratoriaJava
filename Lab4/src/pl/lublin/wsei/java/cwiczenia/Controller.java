de07ee75aa8153ca0ca5178e3aa9278b9c740a3f
Olgierd Tymosiewicz <tymek1667@gmail.com>
2021-04-28 14:33:36 +0200

    [LAB-4b] Zadania 1-1f

--- Lab4/src/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ Lab4/src/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -1,4 +1,11 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class Controller {
+    public Label lbFile;
+
+    public void btnOpenFileAction(ActionEvent actionEvent) {
+    }
 }
