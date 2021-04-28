afc66a8c62da6cbf0189e1c7dae0f8d69663bdd9
Olgierd Tymosiewicz <tymek1667@gmail.com>
2021-04-28 16:24:50 +0200

    [LAB-4b] Zadanie 7

--- Lab4/src/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ Lab4/src/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -6,6 +6,9 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -15,6 +18,7 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Controller {
     private Infografika selInfografika;
@@ -78,6 +82,26 @@ public class Controller {
     public void btnZaladujStrone(ActionEvent actionEvent) {
         if(selInfografika != null) {
             hostServices.showDocument(selInfografika.adresStrony);
+        }
+    }
+
+    public void btnPokazOnAction(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("imgViewer.fxml"));
+            Parent root = loader.load();
+            ImgViewer viewer = loader.getController();
+            if(selInfografika != null) {
+                Image img = new Image(selInfografika.adresGrafiki);
+                viewer.imgView.setFitWidth(img.getHeight());
+                viewer.imgView.setFitHeight(img.getHeight());
+                viewer.imgView.setImage(img);
+            }
+            Stage stage = new Stage();
+            stage.setTitle("Podgląd infografiki");
+            stage.setScene(new Scene(root, 900, 800));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
