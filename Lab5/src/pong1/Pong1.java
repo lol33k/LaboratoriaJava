f5c335e7054b87bebc2c0bf431477e89860a4893
Olgierd Tymosiewicz <tymek1667@gmail.com>
2021-04-28 17:07:06 +0200

    [LAB-5] Zadanie 2 - poprawka

--- Lab5/src/pong1/Pong1.java
+++ Lab5/src/pong1/Pong1.java
@@ -12,10 +12,10 @@ import javafx.stage.Stage;
 public class Pong1 extends Application {
 
     private static final double WIDTH = 800;
-    private static final double HEIGHT = 800;
-    private static final double MARGIN = 800;
-    private static final double ARENAWIDTH = WIDTH - 2*MARGIN;
-    private static final double ARENAHEIGHT = HEIGHT - 2*MARGIN;
+    private static final double HEIGHT = 600;
+    private static final double MARGIN = 50;
+    private static final double ARENAWIDTH = WIDTH - 2 * MARGIN;
+    private static final double ARENAHEIGHT = HEIGHT - 2 * MARGIN;
     private static final double ARENAX1 = MARGIN;
     private static final double ARENAY1 = MARGIN;
     private static final double ARENAX2 = ARENAX1 + ARENAWIDTH;
@@ -23,7 +23,7 @@ public class Pong1 extends Application {
     private static final double R = 10;
 
     @Override
-    public void start(Stage stage) throws Exception{
+    public void start(Stage stage) {
         Canvas canvas = new Canvas(WIDTH,HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
@@ -36,6 +36,7 @@ public class Pong1 extends Application {
 
         gc.setFill(Color.WHITESMOKE);
         gc.fillOval(ARENAX1 + ARENAWIDTH / 2, ARENAY1 + ARENAHEIGHT / 2, 2 * R, 2 * R);
+
     }
 
 
