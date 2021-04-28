969273bfef5ab58e4bc5bcb824c591ed07736368
Olgierd Tymosiewicz <tymek1667@gmail.com>
2021-04-28 17:01:31 +0200

    [LAB-5] Zadanie 2

--- Lab5/src/pong1/Pong1.java
+++ Lab5/src/pong1/Pong1.java
@@ -11,9 +11,20 @@ import javafx.stage.Stage;
 
 public class Pong1 extends Application {
 
+    private static final double WIDTH = 800;
+    private static final double HEIGHT = 800;
+    private static final double MARGIN = 800;
+    private static final double ARENAWIDTH = WIDTH - 2*MARGIN;
+    private static final double ARENAHEIGHT = HEIGHT - 2*MARGIN;
+    private static final double ARENAX1 = MARGIN;
+    private static final double ARENAY1 = MARGIN;
+    private static final double ARENAX2 = ARENAX1 + ARENAWIDTH;
+    private static final double ARENAY2 = ARENAY1 + ARENAHEIGHT;
+    private static final double R = 10;
+
     @Override
     public void start(Stage stage) throws Exception{
-        Canvas canvas = new Canvas(800,600);
+        Canvas canvas = new Canvas(WIDTH,HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
         stage.setTitle("Kulki!");
@@ -21,7 +32,10 @@ public class Pong1 extends Application {
         stage.show();
 
         gc.setFill(Color.BLACK);
-        gc.fillRect(50,50,700,500);
+        gc.fillRect(ARENAX1,ARENAY1,ARENAWIDTH,ARENAHEIGHT);
+
+        gc.setFill(Color.WHITESMOKE);
+        gc.fillOval(ARENAX1 + ARENAWIDTH / 2, ARENAY1 + ARENAHEIGHT / 2, 2 * R, 2 * R);
     }
 
 
