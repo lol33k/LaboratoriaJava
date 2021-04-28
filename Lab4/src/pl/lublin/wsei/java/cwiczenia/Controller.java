1b4965f3606a5aeda6e091786bad498d181b3343
Olgierd Tymosiewicz <tymek1667@gmail.com>
2021-04-28 14:53:54 +0200

    [LAB-4b] Zadanie 4b

--- Lab4/src/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ Lab4/src/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -1,11 +1,38 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class Controller {
     public Label lbFile;
 
+    FileChooser fileChooser = new FileChooser();
+    FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("Pliki XML (*.xml)", "*.xml");
+
+    public ListView lstInfografiki;
+    ObservableList<String> tytuly = FXCollections.observableArrayList();
+    GusInfoGraphicList igList;
+
+    public void initialize() {
+        fileChooser.getExtensionFilters().add(xmlFilter);
+    }
+
     public void btnOpenFileAction(ActionEvent actionEvent) {
+        File file = fileChooser.showOpenDialog(null);
+        if(file != null) {
+            igList = new GusInfoGraphicList(file.getAbsolutePath());
+            lbFile.setText(file.getAbsolutePath());
+            for(Infografika ig: igList.infografiki) tytuly.add(ig.tytul);
+            lstInfografiki.setItems(tytuly);
+        }
+        else {
+            lbFile.setText("Proszę wczytać plik ...");
+        }
     }
 }
