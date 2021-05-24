2647c189b0f5abeefd720672c7366be051dc13e2
Olgierd Tymosiewicz <tymek1667@gmail.com>
2021-05-24 15:25:13 +0200

    [LAB-6] Zadanie 6

--- Lab6/src/pl/lublin/wsei/java/cwiczenia/console/TestBinCopy.java
+++ Lab6/src/pl/lublin/wsei/java/cwiczenia/console/TestBinCopy.java
@@ -9,8 +9,10 @@ public class TestBinCopy {
         FileInputStream in = null;
         FileOutputStream out = null;
 
+        long startTime = System.nanoTime();
+
         try {
-            in = new FileInputStream("sample_1920x1080.tiff");
+            in = new FileInputStream("sample_1920×1280.tiff");
             out = new FileOutputStream("img_copy.tiff");
             int c;
 
@@ -30,5 +32,10 @@ public class TestBinCopy {
                 out.close();
             }
         }
+        long endTime = System.nanoTime();
+        long timeElapsed = endTime - startTime;
+
+        System.out.println("Czas wykonania w nanosekundach : " + timeElapsed);
+        System.out.println("Czas wykonania w milisekundach : " + timeElapsed / 1000000);
     }
 }
